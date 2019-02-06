95c1054c8b2ea38952303b71fcfb9ed7bdcdfee6
danny981118 <Scy981118>
2019-02-06 18:18:39 +0800

    Update vpsSetup.sh

--- vpsSetup.sh
+++ vpsSetup.sh
@@ -3,7 +3,7 @@ user=
 pass=
 port=
 
-apt install -y aria2 screen software-properties-common wget
+apt install -y screen software-properties-common wget
 wget "https://raw.githubusercontent.com/kanzetu/vpsSetup/master/aria2.conf" -O aria2.conf
 cp aria2.conf /etc/aria2.conf
 touch /etc/aria2.session
@@ -27,6 +27,7 @@ apt install -y nodejs
 screen -Sdm aria2 sudo aria2c --conf-path /etc/aria2.conf
 
 wget "https://github.com/porjo/youtubeuploader/releases/download/18.15/youtubeuploader_linux_amd64.tar.gz"
+tar xf youtubeuploader_linux_amd64.tar.gz
 mv youtubeuploader_linux_amd64 /usr/bin/youtubeuploader
 chmod 777 /usr/bin/youtubeuploader
 
@@ -35,8 +36,8 @@ mkdir /home/Download/start
 mkdir /home/Download/upload
 curl -s "https://raw.githubusercontent.com/kanzetu/vpsSetup/master/auto.sh" > /home/Download/auto.sh
 chmod 777 /home/Download/auto.sh
-screen -Sdm auto "sudo /home/Download/auto.sh"
-screen -Sdm cloud "cd /home/Download && cloud-torrent -p $port -a $user:$pass" 
+screen -Sdm  "sudo /home/Download/auto.sh"
+screen -Sdm cloud "cd /home/Download && sudo cloud-torrent -p $port -a $user:$pass" 
 curl -s "https://raw.githubusercontent.com/kanzetu/vpsSetup/master/upload.sh" >  /home/Download/upload/upload.sh
 chmod 777 /home/Download/upload/upload.sh
 
